f3a9d5a44584d94c48c9e84d47a83e7241250f00
matty <mattreed1890@gmail.com>
2025-11-30 22:28:10 -0600

    add texture for car

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include "game.h"
+#include "texture.h"
+#include <stdbool.h>
 #include <stdlib.h>
 
 int main() {
-  run(false);
+  init(true);
+  GLuint texture_id = load_texture("resources/Audi.png");
+  run(texture_id);
   return EXIT_SUCCESS;
 }

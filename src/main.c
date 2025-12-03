c1ce87095f28c76bcce4e696b5251d405f2258ff
matty <mattreed1890@gmail.com>
2025-12-02 22:24:48 -0600

    rename funcs

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 int main() {
-  init(false, 1920, 1080);
+  ope_init(false, 1920, 1080);
   GLuint texture_id = load_texture("resources/Audi.png");
-  run(texture_id);
+  ope_run(texture_id);
   return EXIT_SUCCESS;
 }
